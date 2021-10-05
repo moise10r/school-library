1966b9b49845e8473f58ee145dca8ae91a9e6348
moise10r <moiserushanika2006@gmail.com>
2021-10-06 01:14:55 +0200

    Fix linter errors.

--- person.rb
+++ person.rb
@@ -11,13 +11,13 @@ class Person
     @parent_permission = parent_permission
   end
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
-  private :is_of_age?
+  private :of_age?
 end
