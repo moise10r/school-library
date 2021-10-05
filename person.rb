7b66fcf36869460601ad301bcbee5a6a3ee9cd27
moise10r <moiserushanika2006@gmail.com>
2021-10-05 23:07:16 +0200

    Refactor person class.

--- person.rb
+++ person.rb
@@ -1,23 +1,27 @@
+require 'securerandom'
 
 class Person
-  attr_accessor :name, :age
-  attr_reader :id
+  attr_accessor :id, :name, :age
 
-  def initialize(age:, name: 'Unknown', parent_permission: true)
-    @id = Random.rand(1..1000)
+  def initialize(age, name = 'Unknown', parent_permission = true)
+    @id = SecureRandom.hex(13)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-  # private
 
-  # def of_age?
-  #   @age >= 18
-  # end
-  
   def can_use_services?
     is_of_age? || @parent_permission
   end
 
+  private
+
+  def is_of_age?
+    @age >= 18
+  end
 
 end
+
+person = Person.new(21, "Moise",true)
+puts person.name
+puts person.can_use_services = true
