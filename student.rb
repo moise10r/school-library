c373edde4f9c3ef6709d71ecdf99598b42efc3dc
moise10r <moiserushanika2006@gmail.com>
2021-10-06 00:46:35 +0200

    Refactor student class.

--- student.rb
+++ student.rb
@@ -2,13 +2,12 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(classroom)
-    super
+  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
+    super(age: age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
+  
   def play_hooky
     "¯\(ツ)/¯"
   end
 end
-s= Student.new(4)
-puts s.parent_permission
