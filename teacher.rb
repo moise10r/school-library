812a7a6c58b83b0cfbe763cc41bc7b1d98fb00f6
moise10r <moiserushanika2006@gmail.com>
2021-10-06 00:48:32 +0200

    Refactor teach class.

--- teacher.rb
+++ teacher.rb
@@ -2,15 +2,12 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(classroom)
-    super
-    @classroom = classroom
+  def initialize(age:, specialization:, name: 'Unknown', parent_permission: true)
+    super(age: age, name: name, parent_permission: parent_permission)
+    @specialization = specialization
   end
 
-  def play_hooky
-    "¯\(ツ)/¯"
+  def can_use_services?
+    true
   end
 end
-
-student =  Teacher.new("Moise");
-puts student.name =" Moise"
