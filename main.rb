0e08371ac635a770e52b4fc0419cc02fc6898359
moise10r <moiserushanika2006@gmail.com>
2021-10-08 23:58:04 +0200

    Add create person  method on App class.

--- main.rb
+++ main.rb
@@ -78,4 +78,22 @@ class App
     classroom = Classroom.new('Learn to code')
     @persons.push(Student.new(age: age, classroom: classroom, name: name, parent_permission: parent_permission))
   end
+
+  def create_person
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    person_type = 0
+
+    person_type = gets.chomp while person_type != '2' && person_type != '1'
+
+    case person_type
+    when '1'
+      create_student
+
+    when '2'
+      create_teacher
+    end
+    puts 'Person created successfully'
+    puts
+    run
+  end
 end
