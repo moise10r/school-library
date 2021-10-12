5ac92239c25404d23c4cfd6ad27c116eb654c044
Akasha <Akasha.Rojee@gmail.com>
2021-10-12 18:08:22 +0400

    Add method for selecting menu option

--- main.rb
+++ main.rb
@@ -16,28 +16,32 @@ def show_options
   gets.chomp.to_i
 end
 
-  def run
-    choice = show_options
-    choice = show_options while choice < 1 || choice > 7
-    library = App.new
+def select_option(choice)
+  library = App.new
 
-    case choice
-    when 1
-      library.handle_book_list
-    when 2
-      library.handle_person_list
-    when 3
-      library.create_person
-    when 4
-      library.create_book
-    when 5
-      library.create_rental
-    when 6
-      library.handle_rentals_list
-    when 7
-      puts 'Thank you for using this app!'
-    end
+  case choice
+  when 1
+    library.handle_book_list
+  when 2
+    library.handle_person_list
+  when 3
+    library.create_person
+  when 4
+    library.create_book
+  when 5
+    library.create_rental
+  when 6
+    library.handle_rentals_list
+  when 7
+    puts 'Thank you for using this app!'
   end
+end
+
+def run
+  choice = show_options
+  choice = show_options while choice < 1 || choice > 7
+  select_option(choice)
+end
 
 def main
   puts 'Welcome to School Library App!'
