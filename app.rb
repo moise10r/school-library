febe1e9e38bbdd3063f8eabae637cc3ceebe0980
moise10r <moiserushanika2006@gmail.com>
2021-10-12 15:47:10 +0200

    Remove unnecessary code.

--- app.rb
+++ app.rb
@@ -12,46 +12,6 @@ class App
     @rentals = []
   end
 
-  # rubocop:disable Metrics/CyclomaticComplexity
-  def run
-    choice = show_options
-    choice = show_options while choice < 1 || choice > 7
-
-    case choice
-    when 1
-      handle_book_list
-    when 2
-      handle_person_list
-    when 3
-      create_person
-    when 4
-      create_book
-    when 5
-      create_rental
-    when 6
-      handle_rentals_list
-    when 7
-      puts 'Thank you for using this app!'
-    end
-  end
-  # rubocop:enable Metrics/CyclomaticComplexity
-
-  def show_options
-    options = [
-      '1 - list all books',
-      '2 - List all people',
-      '3 - Create a person',
-      '4 - Create a book',
-      '5 - Create a rental',
-      '6 - List all rentals for a given person id',
-      '7 - exit'
-    ]
-    puts
-    puts 'Please choose an option by entering a number: '
-    puts options
-    gets.chomp.to_i
-  end
-
   def handle_book_list
     @books.each { |book| puts "Title: '#{book.title}', Author: #{book.author}" }
     run
