80e0fb87e85b08b7fbe2810b6e5df33c3dd8a8fe
Akasha <Akasha.Rojee@gmail.com>
2021-10-13 21:11:22 +0400

    Use JSON format for files

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require_relative 'student'
 require_relative 'teacher'
 require_relative 'book'
 require_relative 'rental'
+require 'json'
 
 class App
   def initialize
@@ -104,14 +105,72 @@ class App
   end
 
   def load_files
-    File.foreach('books.txt') { |book| @books.push(book) } if File.exist?('books.txt')
-    File.foreach('persons.txt') { |person| @persons.push(person) } if File.exist?('persons.txt')
-    File.foreach('rentals.txt') { |rental| @rentals.push(rental) } if File.exist?('rentals.txt')
+    @books = load_books
+    load_persons
+    load_rentals(@persons, @books)
+    # File.foreach('rentals.txt') { |rental| @rentals.push(rental) } if File.exist?('rentals.txt')
+  end
+
+  def load_books
+    if File.exist?('books.json')
+      JSON.parse(File.read('books.json'), create_additions: true)
+    else
+      []
+    end
+  end
+
+  def load_persons
+    if File.exist?('persons.json')
+      JSON.parse(File.read('persons.json')).map do |person|
+        if person['json_class'] == 'Student'
+          load_student(person, 'learn to code')
+        else
+          load_teacher(person)
+        end
+      end
+    else
+      @persons = []
+    end
+  end
+
+  def load_teacher(person)
+    id = person['id'].to_i
+    name = person['name']
+    age = person['age']
+    specialization = person['specialization']
+
+    teacher = Teacher.new(age, specialization, name)
+    teacher.id = id
+    @persons.push(teacher)
+  end
+
+  def load_student(person, classroom)
+    id = person['id'].to_i
+    name = person['name']
+    age = person['age']
+    parent_permission = person['parent_permission']
+
+    student = Student.new(age, classroom, name, parent_permission)
+    student.id = id
+    @persons.push(student)
+  end
+
+  def load_rentals(persons, books)
+    if File.exist?('rentals.json')
+      JSON.parse(File.read('rentals.json')).map do |rental|
+        book = books.find { |curr_book| curr_book.title == rental['book'] }
+        person = persons.find { |curr_person| curr_person.id == rental['person'].to_i }
+
+        @rentals.push(Rental.new(rental['date'], person, book))
+      end
+    else
+      @rentals = []
+    end
   end
 
   def save_files
-    File.write('books.txt', @books.join("\n")) if @books.any?
-    File.write('persons.txt', @persons.join("\n")) if @persons.any?
-    File.write('rentals.txt', @rentals.join("\n")) if @rentals.any?
+    File.write('books.json', JSON.generate(@books)) if @books.any?
+    File.write('persons.json', JSON.generate(@persons)) if @persons.any?
+    File.write('rentals.json', JSON.generate(@rentals)) if @rentals.any?
   end
 end
