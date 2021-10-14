b07e4d6231a919fa7990a8b9eaf9eec8bce1ecad
moise10r <moiserushanika2006@gmail.com>
2021-10-14 18:38:19 +0200

    Add the end key word

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,22 +5,22 @@ describe Book do
     @book = Book.new('Game of thrones', 'Moise')
   end
 
-describe 'new' do
-  it 'returns a Book object' do
-    expect(@book).to be_an_instance_of Book
+  describe '#new' do
+    it 'returns a Book object' do
+      expect(@book).to be_an_instance_of Book
+    end
   end
-end
 
 
-describe 'title' do
-  it 'returns the title' do
-    expect(@book.title).to eq 'Game of thrones'
-  end
-  end
-end
+  describe '#title' do
+    it 'returns the title' do
+      expect(@book.title).to eq 'Game of thrones'
+    end
+    end
 
-describe 'author' do
-  it 'returns the author' do
-    expect(@book.author).to eq 'Moise'
+  describe '#author' do
+    it 'returns the author' do
+      expect(@book.author).to eq 'Moise'
+    end
   end
 end
