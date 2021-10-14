4fc641e0b51696d09e03f5cfa5e58940203c9cdb
moise10r <moiserushanika2006@gmail.com>
2021-10-14 21:44:57 +0200

    Add students test

--- student.rb
+++ student.rb
@@ -1,12 +1,14 @@
 require_relative 'person'
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    "¯\\(ツ)/¯"
   end
 
   def classroom=(classroom)
