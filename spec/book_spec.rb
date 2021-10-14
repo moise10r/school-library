f9680715b8e1950f81ee256c1301db4487b9b295
moise10r <moiserushanika2006@gmail.com>
2021-10-14 18:30:13 +0200

    Add new describe for book instance

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,3 +5,9 @@ describe Book do
     @book = Book.new('Game of thrones', 'Moise')
   end
 
+describe '#new' do
+  it 'returns a Book object' do
+    expect(@book).to be_an_instance_of Book
+  end
+end
+
