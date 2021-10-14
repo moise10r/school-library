0c5bc96b30c3f23cac9bd27577585f90e59643ae
moise10r <moiserushanika2006@gmail.com>
2021-10-14 18:59:55 +0200

    Test the created instance of classroom class

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -6,4 +6,10 @@ describe Classroom do
     @classroom = Classroom.new('Learn to code')
   end
 
+  describe '#new' do
+    it 'takes one parameter and returns a Classroom object' do
+      expect(@classroom).to be_an_instance_of Classroom
+    end
+  end
+
 end
