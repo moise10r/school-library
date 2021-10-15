9af5e73a6a4981c4510b54d735d4ed62e04c70b6
Akasha <Akasha.Rojee@gmail.com>
2021-10-15 12:42:37 +0400

    Test teacher for services

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -6,4 +6,7 @@ describe Teacher do
     it 'returns a Teacher object' do
       expect(@teacher).to be_an_instance_of Teacher
     end
+    it 'should return true' do
+      expect(@teacher.can_use_services?).to be(true)
+    end
 end
