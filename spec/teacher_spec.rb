e2a407ae493944431021d68b83dbe2c20db8abcf
Akasha <Akasha.Rojee@gmail.com>
2021-10-15 12:42:16 +0400

    Test teacher object

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -3,4 +3,7 @@ describe Teacher do
     before :each do
       @teacher = Teacher.new(age: 50, specialization: 'Neurologist')
     end
+    it 'returns a Teacher object' do
+      expect(@teacher).to be_an_instance_of Teacher
+    end
 end
